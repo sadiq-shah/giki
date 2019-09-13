2197fae65726cff1c945a880279a3b7f6cee8bff
Muhammad Kashif <44mkashif@gmail.com>
2019-09-14 01:07:40 +0500

    slug validation removed

--- server/validation/faculty.js
+++ server/validation/faculty.js
@@ -2,8 +2,7 @@ const Joi = require('joi');
 
 module.exports = (faculty, update) => {
     let schema = Joi.object().keys({
-      name: Joi.string().min(5).max(100).required(),
-      slug: Joi.string().min(5).max(120)
+      name: Joi.string().min(5).max(100).required()
     });
 
     if(update) {
--- server/validation/page.js
+++ server/validation/page.js
@@ -3,13 +3,12 @@ const Joi = require('joi');
 module.exports = (faculty, update) => {
     let schema = Joi.object().keys({
       name: Joi.string().required(),
-      slug: Joi.string().required(),
       image: Joi.string().required(),
       content: Joi.string()
     });
 
     if(update) {
-      schema = schema.optionalKeys("name","slug","image");
+      schema = schema.optionalKeys("name","image");
     }
   
     return Joi.validate(faculty, schema);
