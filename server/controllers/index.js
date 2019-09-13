ec589aefed5c34a0c9290294f26822c96eed5c3b
Muhammad Kashif <44mkashif@gmail.com>
2019-09-14 00:48:20 +0500

    Page Endpoint created

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,12 @@ const Faculty = require("./faculty");
 const FacultyMember = require("./faculty_member");
 const FacultyImage = require("./faculty_image");
 const UserController = require("./user");
+const Page = require("./page");
    
 module.exports = {
     Faculty,
     FacultyMember,
     FacultyImage,
-    UserController
+    UserController,
+    Page
 }
--- server/validation/index.js
+++ server/validation/index.js
@@ -2,10 +2,12 @@ const Faculty = require("./faculty");
 const Faculty_Member = require("./faculty_member");
 const Faculty_Image = require("./faculty_image");
 const User = require("./user");
+const Page = require("./page");
    
 module.exports = {
     Faculty,
     Faculty_Member,
     Faculty_Image,
-    User
+    User,
+    Page
 }
